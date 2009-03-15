05ff264d9beeba0ab1c271dfe87948238bdef67f
Dan Humphrey <danhumphrey@users.noreply.github.com>
2009-03-15 23:53:55 +0000

    Added new filters

---
+++ classes/Input/Filters/LowerCaseFilter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+  * LowerCaseFilter class
+  * 
+  * @package tmLib
+  * @subpackage InputFilters
+  * @author Dan Humphrey <dan.humphrey@technomedia.co.uk>
+  */
+
+/**
+ * LowerCaseFilter class for ensuring that a request variable is lower case.
+ * 
+ * @package tmLib
+ * @subpackage InputFilters
+ * @author Dan Humphrey <dan.humphrey@technomedia.co.uk>
+ * @access public
+ * @version 1.0
+ */
+class LowerCaseFilter extends BaseFilter{
+
+	/**
+     * Constructs the LowerCaseFilter class 
+     * @access public
+     * @param string $fieldName (optional) the field name of the request variable
+     */
+	function __construct(){
+		parent::__construct();
+	}
+	
+	/**
+	 * Processes the filter and sets the dataset value
+	 * @access public
+	 * @param object DataSet $dataSet the context data space for getting and setting data
+	 */
+	function process($dataSet) {
+		$dataSet->set($this->fieldName, strtolower($dataSet->get($this->fieldName)));
+	}
+}
+?>
---
+++ classes/Input/Filters/UpperCaseFilter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+  * UpperCaseFilter class
+  * 
+  * @package tmLib
+  * @subpackage InputFilters
+  * @author Dan Humphrey <dan.humphrey@technomedia.co.uk>
+  */
+
+/**
+ * UpperCaseFilter class for ensuring that a request variable is upper case.
+ * 
+ * @package tmLib
+ * @subpackage InputFilters
+ * @author Dan Humphrey <dan.humphrey@technomedia.co.uk>
+ * @access public
+ * @version 1.0
+ */
+class UpperCaseFilter extends BaseFilter{
+
+	/**
+     * Constructs the UpperCaseFilter class 
+     * @access public
+     * @param string $fieldName (optional) the field name of the request variable
+     */
+	function __construct(){
+		parent::__construct();
+	}
+	
+	/**
+	 * Processes the filter and sets the dataset value
+	 * @access public
+	 * @param object DataSet $dataSet the context data space for getting and setting data
+	 */
+	function process($dataSet) {
+		$dataSet->set($this->fieldName, strtoupper($dataSet->get($this->fieldName)));
+	}
+}
+?>
